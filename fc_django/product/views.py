d70728faffff3157750c10c03fac5704695ccfb7
poiuyy0420 <poiuyy0420@gmail.com>
2021-01-16 17:31:38 +0900

    order

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
+from django.views.generic.edit import FormView
+from .models import Product
+from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product_list'
+
+
+class ProductCreate(FormView):
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
+
+
+class ProductDeatil(DeleteView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = OrderForm(self.request)
+        return context
