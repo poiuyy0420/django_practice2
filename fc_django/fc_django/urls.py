bf42cf7a58805c9952afbc4ee539688412b7680b
poiuyy0420 <poiuyy0420@gmail.com>
2021-01-16 14:54:21 +0900

    register

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from fc_django.fcuser.views import index
-from fcuser.views import index 
+from fcuser.views import index, RegisterView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index) 
+    path('', index),
+    path('register/', RegisterView.as_view())
 ]
 
