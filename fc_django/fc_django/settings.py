9193fd55196d2f9063e1c0a5cd0e6cc52714a12b
poiuyy0420 <poiuyy0420@gmail.com>
2021-01-17 19:07:56 +0900

    centOS 서버 업로드시 변경

--- fc_django/fc_django/settings.py
+++ fc_django/fc_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7hlbi)0+h&ygwx22ga^9ve$_wjn(y6d1*!cw!+1ycf)r@s$^_o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'fc_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
